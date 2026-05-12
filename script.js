6792c823680a74dc835f05f43fc6c5120bb141d9
Lewi1006 <coach.wind11@gmail.com>
2026-05-12 18:09:49 +0200

    feat: add notes

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // display notes:
 // I need notes to display --> in an array
 
-let notes = ["clean kitchen", "book train ticket" ];
+let notes = ['clean kitchen','book train ticket' ];
 
 // define where notes are displayed
 // when are notes displayed
 
 function renderNotes(){
 let contentRef = document.getElementById('content');
-contentRef.innerHTML += notes
+
+contentRef.innerHTML = ""; 
+
+for(let i = 0; i < notes.length; i++){
+    const note = notes[i];
+    contentRef.innerHTML += getNoteTemplate(note);
+
+}
 }
 
-
 // add notes
+function getNoteTemplate(note){
+    return `<p>+ ${note}</p>`;
+}
+
 // delete notes
+
 // archive notes
