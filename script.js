722552da578e7313cd6b58f5f4a22a5786d0e2e3
Lewi1006 <coach.wind11@gmail.com>
2026-05-12 17:49:32 +0200

    feat: fonts implemented

---
+++ script.js
@@ -0,0 +1,17 @@
+// display notes:
+// I need notes to display --> in an array
+
+let notes = ["clean kitchen", "book train ticket" ];
+
+// define where notes are displayed
+// when are notes displayed
+
+function renderNotes(){
+let contentRef = document.getElementById('content');
+contentRef.innerHTML += notes
+}
+
+
+// add notes
+// delete notes
+// archive notes
